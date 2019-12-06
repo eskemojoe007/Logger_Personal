cde948fc35b4f1b042b81468ab6556ee0e8e5b99
eskemojoe007 <David.Folkner@gmail.com>
2019-12-06 11:49:23 -0500

    Version with Python2 Support

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='colored_logger',
-      version='1.0.7',
+      version='1.0.8',
       description='Colored Logger for terminal',
       url='https://github.com/eskemojoe007/colored_logger',
       author='David Folkner',
